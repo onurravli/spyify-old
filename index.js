15f1d9d8ee29e14c96b3e28f92760500d83f8c6a
onurravli <onurravli@hotmail.com>
2022-12-28 21:23:40 +0300

    Get cookie from user option

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { exit } = require('process');
 const buddyList = require('spotify-buddylist');
 const timestampToDate = require('timestamp-to-date');
 const args = process.argv.slice(2);
@@ -16,7 +17,26 @@ const print = (person) => {
 
 const main = async () => {
     if (args.length < 1) {
-        console.log("usage: spyify [-c <cookie>]");
+        // Take cookie from user
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        readline.question('Enter your cookie: ', async (cookie) => {
+            try {
+                const { accessToken } = await buddyList.getWebAccessToken(cookie);
+                const friendActivity = await buddyList.getFriendActivity(accessToken);
+                const obj = friendActivity["friends"];
+                for (person in obj.reverse()) {
+                    print(obj[person]);
+                }
+                readline.close();
+            } catch (e) {
+                console.log("Invalid cookie");
+                readline.close();
+                exit(-1);
+            }
+        });
     } else {
         if (args[0] === "-c") {
             const cookie = args[1];
