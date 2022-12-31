cc788bcd614e4d30d3513cb8615b3c6e03acbccc
onurravli <onurravli@hotmail.com>
2022-12-31 15:58:01 +0300

    Fix SOME bugs.

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const buddyList = require('spotify-buddylist');
 const timestampToDate = require('timestamp-to-date');
 const args = process.argv.slice(2);
 
-const SP_DC = "";
+let SP_DC = "";
+
+if (process.env.SP_DC) {
+    SP_DC = process.env.SP_DC;
+}
 
 const print = (person) => {
     const name = person["user"]["name"];
@@ -18,46 +22,50 @@ const print = (person) => {
 };
 
 const main = async () => {
-    if (SP_DC.length < 1) {
-        // Take cookie from user
-        const readline = require('readline').createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        readline.question('Enter your cookie: ', async (cookie) => {
-            console.log("");
+    // if no args passed
+    if (args.length === 0) {
+        // if SP_DC is empty
+        if (SP_DC.length === 0) {
+            console.log("usage: spyify [-c <cookie>]");
+            exit(-1);
+        } else {
             try {
-                const { accessToken } = await buddyList.getWebAccessToken(cookie);
+                const { accessToken } = await buddyList.getWebAccessToken(SP_DC);
                 const friendActivity = await buddyList.getFriendActivity(accessToken);
                 const obj = friendActivity["friends"];
                 for (person in obj.reverse()) {
                     print(obj[person]);
                 }
-                readline.close();
-            } catch (e) {
+            } catch {
                 console.log("Invalid cookie");
-                readline.close();
                 exit(-1);
             }
-        });
+        }
     } else {
+        // if args has passed
+        // if args[0] is -c
         if (args[0] === "-c") {
-            const cookie = args[1];
-            const { accessToken } = await buddyList.getWebAccessToken(cookie);
-            const friendActivity = await buddyList.getFriendActivity(accessToken);
-            const obj = friendActivity["friends"];
-            for (person in obj.reverse()) {
-                print(obj[person]);
-            }
-        } else if (SP_DC.length > 0) {
-            const { accessToken } = await buddyList.getWebAccessToken(SP_DC);
-            const friendActivity = await buddyList.getFriendActivity(accessToken);
-            const obj = friendActivity["friends"];
-            for (person in obj.reverse()) {
-                print(obj[person]);
+            // if args[1] is empty
+            if (args[1].length === 0) {
+                console.log("usage: spyify [-c <cookie>]");
+                exit(-1);
+            } else {
+                try {
+                    const cookie = args[1];
+                    const { accessToken } = await buddyList.getWebAccessToken(cookie);
+                    const friendActivity = await buddyList.getFriendActivity(accessToken);
+                    const obj = friendActivity["friends"];
+                    for (person in obj.reverse()) {
+                        print(obj[person]);
+                    }
+                } catch {
+                    console.log("Invalid cookie");
+                    exit(-1);
+                }
             }
         } else {
             console.log("usage: spyify [-c <cookie>]");
+            exit(-1);
         }
     }
 }
